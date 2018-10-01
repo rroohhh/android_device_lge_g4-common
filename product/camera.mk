57506d40ccadf78292051c099307cceef4320e7d
pavlaras <paul@cypheros.co>
2018-10-01 16:39:53 +0200

    g4: camera: Do not build camera hal. We already use prebuilt
    Change-Id: Ia98ef7eb06565c75c40e744c6eb3a713846f389a

--- product/camera.mk
+++ product/camera.mk
@@ -2,7 +2,6 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    camera.msm8992 \
     libqomx_core \
     libmm-qcamera
 
@@ -10,4 +9,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml 
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
